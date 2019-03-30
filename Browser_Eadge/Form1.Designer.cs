288db2ce508004c298c9d53255c5f7872ec21bbd
yohaiag <yohai.agami@gmail.com>
2019-03-30 21:24:51 +0300

    first commit
    first commit

---
+++ Browser_Eadge/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Toolkit.Forms.UI.Controls;
+using System.Windows.Forms;
+
+namespace Browser_Eadge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        //private WebView webView1;
+        private System.ComponentModel.IContainer components = null;
+
+        private ToolStrip toolStrip1, toolStrip2;
+        private ToolStripTextBox toolStripTextBox1;
+        private ToolStripButton goButton, backButton,
+            forwardButton, stopButton, refreshButton,
+            btnCallJavaScript;
+
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.goButton = new System.Windows.Forms.ToolStripButton();
+            this.backButton = new System.Windows.Forms.ToolStripButton();
+            this.forwardButton = new System.Windows.Forms.ToolStripButton();
+            this.stopButton = new System.Windows.Forms.ToolStripButton();
+            this.refreshButton = new System.Windows.Forms.ToolStripButton();
+            this.btnInject = new System.Windows.Forms.ToolStripButton();
+            this.btnCallJavaScript = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.webBrowser1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 50);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(783, 403);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goButton,
+            this.backButton,
+            this.forwardButton,
+            this.stopButton,
+            this.refreshButton,
+            this.btnInject,
+            this.toolStripButton1,
+            this.btnCallJavaScript});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(783, 25);
+            this.toolStrip1.TabIndex = 2;
+            // 
+            // goButton
+            // 
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(26, 22);
+            this.goButton.Text = "Go";
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.Enabled = false;
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(36, 22);
+            this.backButton.Text = "Back";
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Enabled = false;
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(54, 22);
+            this.forwardButton.Text = "Forward";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(35, 22);
+            this.stopButton.Text = "Stop";
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(50, 22);
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // btnInject
+            // 
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(98, 22);
+            this.btnInject.Text = "Inject Java Script";
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // btnCallJavaScript
+            // 
+            this.btnCallJavaScript.Name = "btnCallJavaScript";
+            this.btnCallJavaScript.Size = new System.Drawing.Size(84, 22);
+            this.btnCallJavaScript.Text = "Call javascript";
+            this.btnCallJavaScript.Click += new System.EventHandler(this.btnCallJavaScript_Click);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(783, 25);
+            this.toolStrip2.TabIndex = 1;
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(600, 25);
+            this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 453);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(783, 22);
+            this.statusStrip1.TabIndex = 4;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(129, 22);
+            this.toolStripButton1.Text = "Call c# from javascript";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(783, 475);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webBrowser1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.WebBrowser webBrowser1;
+        private WebView webBrowser1;
+        private ToolStripButton btnInject;
+        private SaveFileDialog saveFileDialog1;
+        private ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Browser_Eadge/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser_Eadge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Settings.IsScriptNotifyAllowed = true;
+            webBrowser1.Settings.IsJavaScriptEnabled = true;
+
+            // The following events are not visible in the designer, so 
+            // you must associate them with their event-handlers in code.
+
+
+            // Load the user's home page.
+            webBrowser1.Navigate(new Uri("https://ynet.co.il/"));
+
+            webBrowser1.FrameContentLoading += WebBrowser1_FrameContentLoading;
+
+
+            
+
+
+
+
+            //Document finished Loading
+            webBrowser1.DOMContentLoaded += WebBrowser1_DOMContentLoaded;
+
+            //When starting to navigate
+            webBrowser1.NavigationStarting += WebBrowser1_NavigationStarting;
+
+            //Navigation Completed
+            webBrowser1.NavigationCompleted += WebBrowser1_NavigationCompleted;
+
+
+            //download file
+            webBrowser1.UnviewableContentIdentified += WebBrowser1_UnviewableContentIdentified;
+
+            webBrowser1.ContextMenuChanged += WebBrowser1_ContextMenuChanged;
+
+            webBrowser1.NewWindowRequested += WebBrowser1_NewWindowRequested;
+
+
+            webBrowser1.ScriptNotify += WebBrowser1_ScriptNotify;
+
+
+            
+            
+
+            
+
+        }
+
+        private void WebBrowser1_NavigationCompleted(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void WebBrowser1_NewWindowRequested(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNewWindowRequestedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void WebBrowser1_UnviewableContentIdentified(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlUnviewableContentIdentifiedEventArgs e)
+        {
+
+            var wc = new WebClient();
+
+
+
+
+            wc.Headers.Add("Accept" , "text/html,application/xhtml+xml,application/xml, application/zip;q=0.9,*/*;q=0.8");
+
+            wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.1 (KHTML, like Gecko)");
+
+
+
+            var fileNameToSave = e.Uri.Segments.Last();
+            saveFileDialog1.FileName = fileNameToSave;
+            var fileEnding = fileNameToSave.Split('.').Last();
+
+
+
+
+
+            
+
+            // Set the file dialog to filter for graphics files.
+            this.saveFileDialog1.Filter =
+                $"File (*.{fileEnding})|*.{fileEnding}|All files (*.*)|*.*";
+
+            var fileurl =
+            e.Uri.AbsoluteUri;
+            
+
+            // Allow the user to select multiple images.
+            
+            this.saveFileDialog1.Title = "Save File Location";
+
+            this.saveFileDialog1.ShowDialog();
+            var filelocation = saveFileDialog1.FileName;
+
+            wc.DownloadFile(fileurl, filelocation);
+        }
+
+        //When starting to navigate
+        private void WebBrowser1_NavigationStarting(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs e)
+        {
+            //MessageBox.Show("start navigation");
+        }
+
+
+        
+
+        private void WebBrowser1_ScriptNotify(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlScriptNotifyEventArgs e)
+        {
+            MessageBox.Show(e.Value);
+        }
+
+        private void WebBrowser1_FrameContentLoading(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlContentLoadingEventArgs e)
+        {
+            string script = @"function magicNumber(return 8);";
+            
+        }
+
+        private void WebBrowser1_ContextMenuChanged(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+
+
+        private async void WebBrowser1_DOMContentLoaded(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlDOMContentLoadedEventArgs e)
+        {
+
+        }
+
+
+
+        // Selects all the text in the text box when the user clicks it. 
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+            toolStripTextBox1.SelectAll();
+        }
+
+        // Navigates to the URL in the address box when 
+        // the ENTER key is pressed while the ToolStripTextBox has focus.
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate(toolStripTextBox1.Text);
+            }
+        }
+
+        // Navigates to the URL in the address box when 
+        // the Go button is clicked.
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(5000);
+            //Navigate(toolStripTextBox1.Text);
+
+        }
+
+        // Navigates to the given URL if it is valid.
+        private void Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://") &&
+                !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+            try
+            {
+                webBrowser1.Navigate(new Uri(address));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+
+      
+
+        // Updates the title bar with the current document title.
+        private void webBrowser1_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            this.Text = webBrowser1.DocumentTitle;
+        }
+
+        // Exits the application.
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private async void btnCallJavaScript_Click(object sender, EventArgs e)
+        {
+
+
+            
+            if (webBrowser1.Source.Host.Contains("ynet"))
+            {
+                try
+                {
+                    string functionString = "function navigateToCnnWhenGoingToYnet(){ window.location.href = 'https://edition.cnn.com/';}";
+                    await webBrowser1.InvokeScriptAsync("eval", new string[] { functionString });
+                    await webBrowser1.InvokeScriptAsync("eval", new string[] { "navigateToCnnWhenGoingToYnet();" });
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+
+
+            
+            string script = @"magicNumber()";
+            string[] args = { script };
+            int foo = int.Parse(webBrowser1.InvokeScript("eval", args));
+            MessageBox.Show(foo.ToString());
+        }
+
+        private async void  toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //string functionString = "function callCSharp(){ window.external.notify(8);}";
+            //await webBrowser1.InvokeScriptAsync("eval", new string[] { functionString });
+            await webBrowser1.InvokeScriptAsync("eval", new string[] { "window.external.notify(\"Hello World\")" });
+        }
+    }
+}
